270abc871af6c29e3ac487ab798b2506f33e0416
Techy Game Bar <65540686+techygamebar@users.noreply.github.com>
2021-04-29 11:13:27 +0530

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,32 @@
+-- CHANGE CORE NAME ACCORDINGLY
+ESX = nil
+
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(200)
+  end
+end)
+
+-- FUNCTION
+function refreshskin()
+  local model = nil
+  local sex = ESX.GetPlayerData().skin.sex
+  if sex == 1 then 
+    model = GetHashKey("mp_f_freemode_01") 
+  else 
+    model = GetHashKey("mp_m_freemode_01") 
+  end 
+  
+  RequestModel(model)
+
+  SetPlayerModel(PlayerId(), model)
+  SetModelAsNoLongerNeeded(model)
+  ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                TriggerEvent('skinchanger:loadSkin', skin)
+end
+
+---COMMAND
+RegisterCommand(Config.Commandname, function()
+  refreshskin()
+end)
